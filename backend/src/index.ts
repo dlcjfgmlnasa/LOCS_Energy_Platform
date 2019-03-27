de05df5f03a9b7d6b2bf6f43d3c69b71bc2934db
dlcjfgmlnasa <dlcjfgmlnasa@naver.com>
2019-03-27 13:56:17 +0900

    디렉토리 변경

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import connectionOptions from "./ormConfig";
 import { createConnection } from "typeorm";
+import helmet from "helmet";
+import cors from "cors";
+import bodyParser from "body-parser";
+
 dotenv.config();
 
-const handleAppStart = () => console.log(`app listening on http://${process.env.HOST_DOMAIN}:${process.env.HOST_PORT}`);
+// jsonwebtoken - Will handle the jwt operations for us
+// bcryptjs - Help us to hash user passwords
+// typeorm - The ORM we are going to use to manipulate database
+// reflect-metadata - allow some annotations features used with TypeORM
+// class-validator - A validation package that works really well with TypeORM
+// ts-node-dev - Automatically restarts the server when we change any file
 
 createConnection(connectionOptions)
   .then(() => {
     // Create a new express application instance
     const app = express();
-    app.listen(Number(process.env.HOST_PORT), String(process.env.HOST_DOMAIN), handleAppStart);
+
+    // Call Middleware
+    app.use(cors());                    /* cors - Enable cross-origin Requests */
+    app.use(helmet());                  /* helmet - Help us to secure our application by setting various HTTP headers */
+    app.use(bodyParser.json());         /* body-parser - Parses the client’s request from json into javascript objects */
+
+    app.listen(Number(process.env.HOST_PORT), String(process.env.HOST_DOMAIN), () => {
+      console.log(`app listening on http://${process.env.HOST_DOMAIN}:${process.env.HOST_PORT}`);
+    });
   })
   .catch(error => console.log(error));
--- backend/src/ormConfig.ts
+++ backend/src/ormConfig.ts
@@ -9,8 +9,9 @@ const ConnectionOptions: ConnectionOptions = {
   password: process.env.DB_PASSWORD || "",
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT),
-  synchronize: true
-//   entities: ["entities/**/*.*"]
+  synchronize: true,
+  logging: true,
+  entities: ["entities/**/*.*"]
 }
 
 export default ConnectionOptions;
